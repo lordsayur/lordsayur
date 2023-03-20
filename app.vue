1c2c806883d6468ce8747ac9acb4ed594ad2aa08
Omar <38073753+lordsayur@users.noreply.github.com>
2023-03-20 05:29:59 +0000

    ✨ update app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="main">
+    <div>
+      <h1>i</h1>
+      <h1>am</h1>
+      <h1>lordsayur 👑</h1>
+    </div>
   </div>
 </template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  box-sizing: border-box;
+  background-color: darkslategray;
+}
+#__nuxt {
+  height: 100%;
+}
+.main {
+  color: goldenrod;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
